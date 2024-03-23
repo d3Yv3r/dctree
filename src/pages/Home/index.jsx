01a6137da27e72b1a8916f681d0a84b5bbb9face
d3Yv3r <deyverson@astrotech.solutions>
2024-03-23 16:52:51 -0300

    Criando componentes de pagina

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <ul>
+      <li>
+        <Link to="/lista-links">Listar links</Link>
+      </li>
+      <li>
+        <Link to="/admin/dashboard">Dashboard</Link>
+      </li>
+    </ul>
+  );
+}
