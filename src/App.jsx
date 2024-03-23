8ecd858939d1806d261d7acdd8aac33b4352cfb0
d3Yv3r <deyverson@astrotech.solutions>
2024-03-23 13:27:36 -0300

    Commit de atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import "./App.css";
+import Link from "./components/Link";
+import LinkList from "./components/LinkList";
+
+const links = [
+  {
+    label: "Quero aprender a programar",
+  },
+  {
+    label: "Quero aprender data analytics",
+  },
+  {
+    label: "Quero aprender marketing",
+  },
+  {
+    label: "Inscreva-se nos nossos cursos",
+    title: "Curso intensivo",
+    description:
+      "Veja aqui quais os cursos intensivos estão com turmas abertas e garanta sua vaga.",
+  },
+  {
+    label: "Clique e se inscreva",
+    title: "Digital Plus",
+    description: "Evento exclusivo para alunos!",
+  },
+];
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <LinkList>
+      {links.map((item) => (
+        <Link {...item} />
+      ))}
+    </LinkList>
+  );
 }
 
 export default App;
