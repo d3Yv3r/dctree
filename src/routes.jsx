d1c3ae1b9ca255109fcf0c19a428fcda5cfb6da0
d3Yv3r <deyverson@astrotech.solutions>
2024-04-06 16:53:20 -0300

    Realizando ajustes de configuração do Bootstrap

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,6 +7,7 @@ import {
 import Home from "@/pages/Home";
 import AccountLinks from "@/pages/AccountLinks";
 import Dashboard from "./pages/Admin/Dashboard";
+import { CreateLink } from "./pages/Admin/CreateLink";
 
 export default createBrowserRouter(
   createRoutesFromElements(
@@ -15,6 +16,7 @@ export default createBrowserRouter(
       <Route path="/:accountlink" element={<AccountLinks />}></Route>
       <Route path="/admin">
         <Route path="dashboard" element={<Dashboard />}></Route>
+        <Route path="create-link" element={<CreateLink />}></Route>
       </Route>
     </>
   )
